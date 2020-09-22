82982188190e9d8a76bc623c8a84d6c6b15ed7bd
geyz201 <43728931+geyz201@users.noreply.github.com>
2020-09-23 02:15:36 +0800

    Update FileHelper.kt

--- app/src/main/java/com/example/sensortest/FileHelper.kt
+++ app/src/main/java/com/example/sensortest/FileHelper.kt
@@ -3,35 +3,28 @@ package com.example.sensortest
 import android.content.Context
 import android.os.Environment
 import android.widget.Toast
-import org.json.JSONArray
-import org.json.JSONObject
-import threeDvector.Vec3D
 import java.io.File
 import java.io.FileNotFoundException
 import java.io.FileOutputStream
 import java.io.IOException
-import java.util.*
 
 /*
  * 这里定义的是一个文件保存的方法，写入到文件中，所以是输出流
  * */
 @Throws(Exception::class)
-fun VecDatasave(mContext: Context, filecontent: ArrayList<Vec3D>, vectorname: String) {
+inline fun Filesave(mContext: Context, fileContent: String, filepath:String?=null,filename: String?) {
     //这里我们使用私有模式,创建出来的文件只能被本应用访问,还会覆盖原文件哦
     if (Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED) {
-        val myExternalFile: File = File(mContext.getExternalFilesDir(null), "SensorDataRecord")
-        val output = FileOutputStream(myExternalFile)
-        val jsonArray = JSONArray()
-        for (item in filecontent.iterator()) {
-            val json = JSONObject()
-            json.put(vectorname + "_X", item[0])
-            json.put(vectorname + "_Y", item[1])
-            json.put(vectorname + "_Z", item[2])
-            jsonArray.put(json)
+        val myExternalFile: File = File(mContext.getExternalFilesDir(filepath), filename)
+        try {
+            val fileOutPutStream = FileOutputStream(myExternalFile)
+            fileOutPutStream.write(fileContent.toByteArray())
+            fileOutPutStream.close()
+            Toast.makeText(mContext, "数据写入成功", Toast.LENGTH_SHORT).show()
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Toast.makeText(mContext, "数据写入失败", Toast.LENGTH_SHORT).show()
         }
-        val s = jsonArray.toString()
-        output.write(s.toByteArray())
-        output.close() //关闭输出流
     } else Toast.makeText(mContext, "SD卡不存在或者不可读写", Toast.LENGTH_SHORT).show()
 }
 
