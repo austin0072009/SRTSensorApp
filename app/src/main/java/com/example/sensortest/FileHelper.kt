50ef64646db7fb74f2d9020af26459c6b0c04c84
geyz201 <43728931+geyz201@users.noreply.github.com>
2020-09-23 00:29:28 +0800

    Update FileHelper.kt

--- app/src/main/java/com/example/sensortest/FileHelper.kt
+++ app/src/main/java/com/example/sensortest/FileHelper.kt
@@ -5,58 +5,51 @@ import android.os.Environment
 import android.widget.Toast
 import org.json.JSONArray
 import org.json.JSONObject
+import threeDvector.Vec3D
+import java.io.File
 import java.io.FileNotFoundException
 import java.io.FileOutputStream
 import java.io.IOException
 import java.util.*
 
-class FileHelper {
-    private var mContext: Context? = null
-
-    constructor() {}
-    constructor(mContext: Context?) : super() {
-        this.mContext = mContext
-    }
-
-    /*
-     * 这里定义的是一个文件保存的方法，写入到文件中，所以是输出流
-     * */
-    @Throws(Exception::class)
-    fun save(filecontent: Vector<FloatArray>) {
-        //这里我们使用私有模式,创建出来的文件只能被本应用访问,还会覆盖原文件哦
-        if (Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED) {
-            val filename = Environment.getExternalStorageDirectory().canonicalPath + "/" + "SensorDataRecord"
-            val output = FileOutputStream(filename)
-            val jsonArray = JSONArray()
-            for (i in filecontent.indices) {
-                val json = JSONObject()
-                json.put("Acceleration_X", filecontent[i][0])
-                json.put("Acceleration_Y", filecontent[i][1])
-                json.put("Acceleration_Z", filecontent[i][2])
-                jsonArray.put(json)
-            }
-            val s = jsonArray.toString()
-            output.write(s.toByteArray())
-            output.close() //关闭输出流
-        } else Toast.makeText(mContext, "SD卡不存在或者不可读写", Toast.LENGTH_SHORT).show()
-    }
-
-    /*
-     * 这里定义的是文件读取的方法
-     * */
-    @Throws(IOException::class, FileNotFoundException::class)
-    fun read(filename: String?): String {
-        //打开文件输入流
-        val input = mContext!!.openFileInput(filename)
-        val temp = ByteArray(1024)
-        val sb = StringBuilder("")
-        var len = 0
-        //读取文件内容:
-        while (input.read(temp).also { len = it } > 0) {
-            sb.append(String(temp, 0, len))
+/*
+ * 这里定义的是一个文件保存的方法，写入到文件中，所以是输出流
+ * */
+@Throws(Exception::class)
+fun VecDatasave(mContext: Context, filecontent: ArrayList<Vec3D>, vectorname: String) {
+    //这里我们使用私有模式,创建出来的文件只能被本应用访问,还会覆盖原文件哦
+    if (Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED) {
+        var myExternalFile: File = File(getExternalFilesDir(null), "SensorDataRecord")
+        val output = FileOutputStream(myExternalFile)
+        val jsonArray = JSONArray()
+        for (item in filecontent.iterator()) {
+            val json = JSONObject()
+            json.put(vectorname + "_X", item[0])
+            json.put(vectorname + "_Y", item[1])
+            json.put(vectorname + "_Z", item[2])
+            jsonArray.put(json)
         }
-        //关闭输入流
-        input.close()
-        return sb.toString()
+        val s = jsonArray.toString()
+        output.write(s.toByteArray())
+        output.close() //关闭输出流
+    } else Toast.makeText(mContext, "SD卡不存在或者不可读写", Toast.LENGTH_SHORT).show()
+}
+
+/*
+ * 这里定义的是文件读取的方法
+ * */
+@Throws(IOException::class, FileNotFoundException::class)
+fun VecDatasave(mContext: Context, filename: String?): String {
+    //打开文件输入流
+    val input = mContext!!.openFileInput(filename)
+    val temp = ByteArray(1024)
+    val sb = StringBuilder("")
+    var len = 0
+    //读取文件内容:
+    while (input.read(temp).also { len = it } > 0) {
+        sb.append(String(temp, 0, len))
     }
+    //关闭输入流
+    input.close()
+    return sb.toString()
 }
