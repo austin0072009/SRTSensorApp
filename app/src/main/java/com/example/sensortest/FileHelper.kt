204d5b8c1779b86ace0aeee2e88ffb52c91fc848
geyz201 <43728931+geyz201@users.noreply.github.com>
2020-09-23 12:17:09 +0800

    Update FileHelper.kt

--- app/src/main/java/com/example/sensortest/FileHelper.kt
+++ app/src/main/java/com/example/sensortest/FileHelper.kt
@@ -4,15 +4,23 @@ import android.app.Activity
 import android.content.Context
 import android.os.Environment
 import android.widget.Toast
+import com.google.gson.Gson
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
 
+/*
+ * 使用GSON将对象转化为JSON
+ * */
+fun serialize(vararg srcs: Any): String {
+    val gson = Gson()
+    return if (srcs.size == 1) gson.toJson(srcs) else gson.toJson(listOf(srcs))
+}
 
 /*
  * 这里定义的是一个文件保存的方法，写入到文件中，所以是输出流
  * */
-fun Activity.FileSave(mContext: Context, fileContent: String, filepath:String?=null, filename: String?) {
+fun Activity.FileSave(mContext: Context, fileContent: String, filepath: String? = null, filename: String) {
     //这里我们使用私有模式,创建出来的文件只能被本应用访问,还会覆盖原文件哦
     if (Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED) {
         val myExternalFile: File = File(mContext.getExternalFilesDir(filepath), filename)
