c59377b4c1a159e10960239bc1f4c3ceb61221de
geyz201 <43728931+geyz201@users.noreply.github.com>
2020-09-26 15:17:30 +0800

    加了些基本的生命周期

--- app/src/main/java/com/example/sensortest/SensorRecord.kt
+++ app/src/main/java/com/example/sensortest/SensorRecord.kt
@@ -39,6 +39,21 @@ class SensorRecord : Service(), SensorEventListener {
         TODO("Not yet implemented")
     }
 
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        return super.onUnbind(intent)
+    }
+
+    override fun onRebind(intent: Intent?) {
+        super.onRebind(intent)
+    }
     override fun onSensorChanged(event: SensorEvent) {
         val range = 1.0 //设定一个精度范围
         val sensor = event.sensor
@@ -55,6 +70,9 @@ class SensorRecord : Service(), SensorEventListener {
         }
     }
 
-    override fun onBind(intent: Intent): IBinder? = null
-
+    override fun onDestroy() {
+        super.onDestroy()
+        sensorManager.unregisterListener(this)
+        m_wkik.release()
+    }
 }
