ed6c4d32180c3e04054e78178526cf3cb40b993e
geyz201 <43728931+geyz201@users.noreply.github.com>
2020-09-27 01:03:30 +0800

    覆盖hhh

--- app/src/main/java/com/example/sensortest/MainActivity.kt
+++ app/src/main/java/com/example/sensortest/MainActivity.kt
@@ -1,29 +1,85 @@
 package com.example.sensortest
 
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.activity_main.*
+import threeDvector.Vec3D
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
-
+    val df = DecimalFormat("####0.00")
+    val AccObserver = Observer<Vec3D> {
+        tv_step.text = "X: " + df.format(it.x)
+        tv_step2.text = "Y: " + df.format(it.y)
+        tv_step3.text = "Z: " + df.format(it.z)
+    }
+    val GRVObserver = Observer<Vec3D> {
+        tv_Gstep.text = "X: " + df.format(it.x)
+        tv_Gstep2.text = "Y: " + df.format(it.y)
+        tv_Gstep3.text = "Z: " + df.format(it.z)
+    }
     var processState = true
+
+
+    private lateinit var mService: SensorRecord
+    private var mBound: Boolean = false
+    private val connection = object : ServiceConnection {
+
+        override fun onServiceConnected(className: ComponentName, service: IBinder) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            val binder = service as SensorRecord.LocalBinder
+            mService = binder.getService()
+            mBound = true
+        }
+
+        override fun onServiceDisconnected(arg0: ComponentName) {
+            mBound = false
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         BindViews()
     }
 
+    /*override fun onStart() {
+        super.onStart()
+        // Bind to LocalService
+        Intent(this, SensorRecord::class.java).also { intent ->
+            bindService(intent, connection, Context.BIND_AUTO_CREATE)
+        }
+        mService.currentAcc.observe(this,AccObserver)
+        mService.currentGRV.observe(this,GRVObserver)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unbindService(connection)
+        mBound = false
+    }*/
+
     private fun BindViews() {
         btn_start.setOnClickListener {
             if (processState) {
-                val startIntent = Intent(this@MainActivity, SensorRecord::class.java)
-                startService(startIntent)
+                val intent=Intent(this, SensorRecord::class.java)
+                startService(intent)
+                bindService(intent, connection, Context.BIND_AUTO_CREATE)
+                mService.currentAcc.observe(this,AccObserver)
+                mService.currentGRV.observe(this,GRVObserver)
                 btn_start.text = "停止"
             } else {
-                val stopIntent = Intent(this@MainActivity, SensorRecord::class.java)
-                stopService(stopIntent)
+                intent = Intent(this, SensorRecord::class.java)
+                unbindService(connection)
+                stopService(intent)
                 btn_start.text = "开始记录数据"
+                mBound = false
             }
             processState = !processState
         }
--- app/src/main/java/com/example/sensortest/SensorRecord.kt
+++ app/src/main/java/com/example/sensortest/SensorRecord.kt
@@ -1,26 +1,46 @@
 package com.example.sensortest
 
+import android.R
+import android.app.Notification
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.os.Binder
+import android.os.Build
 import android.os.IBinder
 import android.os.PowerManager
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.lifecycle.MutableLiveData
 import threeDvector.Vec3D
-import java.text.DecimalFormat
-import java.util.ArrayList
+import java.util.*
+
 
 class SensorRecord : Service(), SensorEventListener {
+    private val binder= LocalBinder()
     private lateinit var sensorManager: SensorManager
     private lateinit var powerManager: PowerManager
     private lateinit var m_wkik: PowerManager.WakeLock
 
     private val sensorData_Acc = ArrayList<Vec3D>()
     private val sensorData_GRV = ArrayList<Vec3D>()
+    private var i = 0
+    val currentAcc: MutableLiveData<Vec3D> by lazy {
+        MutableLiveData<Vec3D>()
+    }
+    val currentGRV: MutableLiveData<Vec3D> by lazy {
+        MutableLiveData<Vec3D>()
+    }
+
+    inner class LocalBinder : Binder() {
+        // Return this instance of LocalService so clients can call public methods
+        fun getService(): SensorRecord = this@SensorRecord
+    }
 
     override fun onCreate() {
         super.onCreate()
@@ -41,7 +61,7 @@ class SensorRecord : Service(), SensorEventListener {
         return super.onStartCommand(intent, flags, startId)
     }
 
-    override fun onBind(intent: Intent?): IBinder? = null
+    override fun onBind(intent: Intent?): IBinder = binder
 
     override fun onUnbind(intent: Intent?): Boolean {
         return super.onUnbind(intent)
@@ -58,14 +78,17 @@ class SensorRecord : Service(), SensorEventListener {
             Sensor.TYPE_LINEAR_ACCELERATION -> {
                 //Data Receive from sensor
                 val tmpVec = Vec3D(event.values)
+                currentAcc.value = tmpVec
                 sensorData_Acc.add(tmpVec)
                 if (sensorData_Acc.size == 300) {
-                    FileSave(fileContent = serialize(sensorData_Acc), filename = "SensorRecord.JSON")
+                    FileSave(fileContent = serialize(sensorData_Acc), filename = "SensorRecord${i}.JSON")
                     sensorData_Acc.clear()
+                    i += 1
                 }
             }
             Sensor.TYPE_GAME_ROTATION_VECTOR -> {
                 val tmpVec = Vec3D(event.values)
+                currentAcc.value = tmpVec
                 sensorData_GRV.add(tmpVec)
             }
         }
